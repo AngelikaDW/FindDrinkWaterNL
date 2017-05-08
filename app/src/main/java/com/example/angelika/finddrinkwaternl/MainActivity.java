15caafc45d150f406538f26b3e54428a1fceb3d3
AngelikaDw <1ngelika>
2017-05-08 11:45:41 +0200

    Splash Screen Added: Logo too small Main Activity: layout updated Translations Dutch and German added

--- app/src/main/java/com/example/angelika/finddrinkwaternl/MainActivity.java
+++ app/src/main/java/com/example/angelika/finddrinkwaternl/MainActivity.java
@@ -95,22 +95,6 @@ public class MainActivity extends AppCompatActivity implements
         CameraPosition target = CameraPosition.builder().target(amsterdamNL).zoom(14).build();
         m_map.moveCamera(CameraUpdateFactory.newCameraPosition(target));
 
-//        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
-//                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
-//
-//        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
-//            @Override
-//            public void onPlaceSelected(Place place) {
-//                // TODO: Get info about the selected place.
-//                Log.i("PlaceAutoComplete", "Place: " + place.getName());
-//            }
-//
-//            @Override
-//            public void onError(Status status) {
-//                // TODO: Handle the error.
-//                Log.i("PlaceAutoComplete", "An error occurred: " + status);
-//            }
-//        });
     }
 
     @Override
